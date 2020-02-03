9d17a67544059b8cfc21a8252e272932344381c3
Abhi Dommalapati <ad4bu@virginia.edu>
2020-02-03 12:36:14 -0500

    this app is officially done

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from polls import views
+
 urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
